b26206ae6d92b63e1c5de280407db2cd3973f772
Igor Petrov <derokarf@gmail.com>
2018-06-18 18:37:00 +0800

    - доделал в добавлении нового участника и лодку и трекер

--- SMRaces.js
+++ SMRaces.js
@@ -80,6 +80,28 @@ const SMRaces = (function() {
         })
         .catch(err => console.log(err));
     },
+    /** @description Получает список участвующих трекеров
+    * @param {idRace} Id гонки
+    * @param {tbGadgets} Объект таблицы GUI
+    */
+    getgadgets(idRace, tbGadgets) {
+      const data = {
+        idrace: idRace
+      };
+      fetch(`${urlApi}/races/getgadgets`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: SMHeaders,
+        body: JSON.stringify(data)
+      })
+        .then(res => res.json())
+        .then(dataSet => {
+          tbGadgets.clear();
+          tbGadgets.rows.add(SMUtils.objArr2arrArr_raw(dataSet));
+          tbGadgets.draw();
+        })
+        .catch(err => console.log(err));
+    },
     /** @description Добавляет запись о гонке
     * @param {data} Данные о гонке
     * @param {tbRaces} Объект таблицы GUI
@@ -93,6 +115,22 @@ const SMRaces = (function() {
       })
         .then(res => {
           this.getall(tbRaces);
+        })
+        .catch(err => console.log(err));
+    },
+    /** @description Добавляет запись об участнике в гонке
+    * @param {data} Данные об участнике
+    * @param {tbRaces} Объект таблицы GUI
+    */
+    addConfig(data, tbRaceConfig) {
+      fetch(`${urlApi}/races/addConfig`, {
+        method: 'POST',
+        mode: 'cors',
+        headers: SMHeaders,
+        body: JSON.stringify(data)
+      })
+        .then(res => {
+          this.getconfig(tbRaceConfig,data.raceConfigIdRace);
         })
         .catch(err => console.log(err));
     }
